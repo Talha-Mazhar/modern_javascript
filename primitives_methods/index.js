e7bd064cbf98223b23700c6c7653ce7483d8b080
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-12-15 01:24:18 +0500

    primitives and obejcts defined

---
+++ primitives_methods/index.js
@@ -0,0 +1,15 @@
+// Primives: store lightweight values also an object but in other sense (boolean, bigint, string, null, undefined, number, symbol)
+// Object: Store multiple different values -> {} {name: "Talha", age: "22"}, Takes resources
+//
+
+// Primitive is also an obeject to provide methods for strings and numbers
+
+// String()
+// Number() returns number
+// new Number() is an object not recommended with new keyword
+
+// str.toUpperCase() method  objectWrapper(str).toUpperCase()
+
+// Object Wrapper for primitives that objectwrapper provides methods
+
+// null undefined there is no method
