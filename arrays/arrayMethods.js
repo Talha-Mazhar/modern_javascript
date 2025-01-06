49113c8bb3e75dff66384e080d1e4a1b936ed51e
Talha-Mazhar <ahmadnutt9111@gmail.com>
2025-01-07 03:00:56 +0500

    task upto 6 completed

--- arrays/arrayMethods.js
+++ arrays/arrayMethods.js
@@ -34,4 +34,87 @@ let filtered = filterRange(arr, 1, 4)
 
 console.log(filtered) // 3,1 (matching values)
 
-console.log(arr) // 5,3,8,1 (not modified)
+// *****************Task 3 *********************
+
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and
+// removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+// The function should only modify the array. It should not return anything.
+
+let arr2 = [5, 3, 8, 1]
+
+function filterRangeInPlace(arr = [], a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i]
+    // remove if outside of the interval
+    if (val < a || val > b) {
+      arr.splice(i, 1)
+      i--
+    }
+  }
+}
+filterRangeInPlace(arr2, 1, 4)
+
+console.log(arr2)
+
+// task 4
+
+// Sort in decreasing order
+let unSortArr = [5, 2, 1, -10, 8]
+
+function sortInDec(arr = []) {
+  return arr.sort((a, b) => b - a)
+}
+
+console.log(sortInDec(unSortArr))
+
+// Task 5
+// We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
+// Create a function copySorted(arr) that returns such a copy.
+
+function copySorted(arr = []) {
+  return arr.slice().sort()
+}
+
+let arr3 = ['HTML', 'JavaScript', 'CSS']
+
+let sorted = copySorted(arr3)
+
+console.log(sorted) // CSS, HTML, JavaScript
+console.log(arr3) // HTML, JavaScript, CSS (no changes)
+
+// Task 6
+// Create a constructor function Calculator that creates “extendable” calculator objects
+
+function Calculator() {
+  this.methods = {
+    '-': (a, b) => a - b,
+    '+': (a, b) => a + b,
+  }
+
+  this.calculate = function (str) {
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2]
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN
+    }
+
+    return this.methods[op](a, b)
+  }
+
+  this.addMethod = function (name, func) {
+    this.methods[name] = func
+  }
+}
+
+let powerCalc = new Calculator()
+powerCalc.addMethod('*', (a, b) => a * b)
+powerCalc.addMethod('/', (a, b) => a / b)
+powerCalc.addMethod('**', (a, b) => a ** b)
+
+let result = powerCalc.calculate('2 ** 3')
+
+let result2 = powerCalc.calculate('2 / 3')
+console.log(result)
