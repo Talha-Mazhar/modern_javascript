12794d089c89078ccc2c01663fe7aa605f71967c
Talha-Mazhar <ahmadnutt9111@gmail.com>
2025-01-07 03:12:52 +0500

    task upto 7 done

--- arrays/arrayMethods.js
+++ arrays/arrayMethods.js
@@ -119,3 +119,21 @@ let result = powerCalc.calculate('2 ** 3')
 let result2 = powerCalc.calculate('2 + 3')
 console.log(result)
 console.log(result2)
+
+// Task 7
+// You have an array of user objects, each one has name, surname and id.
+// Write the code to create another array from it, of objects with id and fullName,
+// where fullName is generated from name and surname.
+
+let john = { name: 'John', surname: 'Smith', id: 1 }
+let pete = { name: 'Pete', surname: 'Hunt', id: 2 }
+let mary = { name: 'Mary', surname: 'Key', id: 3 }
+
+let users = [john, pete, mary]
+let usersMapped = users.map((item) => ({
+  id: item.id,
+  fullName: `${item.name} ${item.surname}`,
+}))
+
+console.log(usersMapped[0].id) // 1
+console.log(usersMapped[0].fullName) // John Smith
