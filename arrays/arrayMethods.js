1b44a8f0f923197ea8c8a0ed8d35aa99f853a57d
Talha-Mazhar <ahmadnutt9111@gmail.com>
2025-01-09 22:46:59 +0500

    Object properties

--- arrays/arrayMethods.js
+++ arrays/arrayMethods.js
@@ -137,3 +137,25 @@ let usersMapped = users.map((item) => ({
 
 console.log(usersMapped[0].id) // 1
 console.log(usersMapped[0].fullName) // John Smith
+
+const objectProperties = {
+  name: 'talha',
+  phone: '1234567890',
+  gender: 'male male male',
+  age: 23,
+  height: 6.1,
+  wealth: 0o0,
+}
+
+console.log(Object.keys(objectProperties)) //[ 'name', 'phone', 'gender', 'age', 'height', 'wealth' ]
+console.log(Object.entries(objectProperties))
+// [
+//   [ 'name', 'talha' ],
+//   [ 'phone', '1234567890' ],
+//   [ 'gender', 'male male male' ],
+//   [ 'age', 23 ],
+//   [ 'height', 6.1 ],
+//   [ 'wealth', 0 ]
+// ]
+console.log(Object.values(objectProperties))
+// [ 'talha', '1234567890', 'male male male', 23, 6.1, 0 ]
