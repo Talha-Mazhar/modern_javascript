0cbb1d5e2061850bf88f27868d90d7f52cc23bf5
Talha-Mazhar <ahmadnutt9111@gmail.com>
2025-01-13 00:45:19 +0500

    protoype task 1

---
+++ prototypes_inheritence/outputTasks.js
@@ -0,0 +1,19 @@
+let animal = {
+  jumps: null,
+};
+let rabbit = {
+  __proto__: animal,
+  jumps: true,
+};
+
+console.log(rabbit.jumps); //output = true
+
+delete rabbit.jumps; //jums -deleted
+
+console.log(rabbit.jumps); //output = null
+
+delete animal.jumps;
+// output: animal = {}
+
+console.log(rabbit.jumps);
+// output undefined no suck proeprty exists
